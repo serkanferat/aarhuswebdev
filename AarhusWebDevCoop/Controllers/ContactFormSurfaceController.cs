7a70f419d4e6a5d256c8e6fbad96a87d748951e9
Serkan Ferat <serkanferat@gmail.com>
2016-03-29 23:03:52 +0200

    Uploaded project

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                //Perhaps you might want to add a custom message to the ViewBag
+                //which will be available on the View when it renders (since we're not 
+                //redirecting)          
+                return CurrentUmbracoPage();
+            }
+
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                TempData["success"] = true;
+                // create message
+                IContent emailMessage = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+
+                emailMessage.SetValue("fullName", model.Name);
+                emailMessage.SetValue("email", model.Email);
+                emailMessage.SetValue("subject", model.Subject);
+                emailMessage.SetValue("message", model.Message);
+
+                //Save
+                Services.ContentService.Save(emailMessage);
+
+                //redirect to current page to clear the form
+                return RedirectToCurrentUmbracoPage();
+            }
+        }
+    }
+
+}
